e87522af34b74d106225befce890fc28d1dad704
Michael Ly <m.ly@live.co.uk>
2018-08-21 15:55:23 +0000

    add movie search and results page

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express"),
+    app     = express(),
+    request = require("request"),
+    bodyParser = require("body-parser");
+
+//APP CONFIG
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//ROUTES
+
+//index/new search form
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+//show search result
+app.get("/results", function(req, res){
+    var movieTitle = req.query.movieSearch;
+    request("http://www.omdbapi.com/?s=" + movieTitle + "&apikey=thewdb", function(error, response, body){
+        if(error){
+            console.log(error);
+            res.redirect("back");
+        } else {
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
+        }
+    });
+});
+
+//movie id show
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Movie App has started");
+});
